81261d2d53b497f35bdec69a2ac24618c52b9d92
Hamlet <55106549+hamletavetisian@users.noreply.github.com>
2021-07-09 09:19:13 -0700

    Update spinal-decompression-therapy-san-diego.js

--- web/src/pages/spinal-decompression-therapy-san-diego.js
+++ web/src/pages/spinal-decompression-therapy-san-diego.js
@@ -202,12 +202,12 @@ const Page = ({ data }) => {
 export const query = graphql`
   {
     openGraphImage: file(
-      relativePath: { eq: "open-graph/facebook/Spinal Decompression.jpg.jpg" }
+      relativePath: { eq: "open-graph/facebook/Spinal Decompression.jpg" }
     ) {
       publicURL
     }
     twitterOpenGraphImage: file(
-      relativePath: { eq: "open-graph/twitter/Spinal Decompression.jpg.jpg" }
+      relativePath: { eq: "open-graph/twitter/Spinal Decompression.jpg" }
     ) {
       publicURL
     }
